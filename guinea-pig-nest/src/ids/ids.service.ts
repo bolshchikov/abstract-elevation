1cb25ece046c46b42dfcbdedee52e4e0a5ae28b8
Sergey Bolshchikov <sergey@bolshchikov.net>
2022-10-28 10:03:17 +0300

    chore(guinea-pig-nest): instrument more calls

--- guinea-pig-nest/src/ids/ids.service.ts
+++ guinea-pig-nest/src/ids/ids.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Span } from 'nestjs-otel';
 
 @Injectable()
 export class IdsService {
   private lastGeneratedId = 0;
+
+  @Span()
   generateId(): number {
     this.lastGeneratedId += 1;
     return this.lastGeneratedId;
--- guinea-pig-nest/src/invoices/model/invoice.ts
+++ guinea-pig-nest/src/invoices/model/invoice.ts
@@ -1,5 +1,7 @@
 import { CreateInvoiceDto } from '../dto/create-invoice.dto';
+import { OtelInstanceCounter } from 'nestjs-otel';
 
+@OtelInstanceCounter()
 export class Invoice {
   constructor(invoiceDto: CreateInvoiceDto) {
     this.amount = invoiceDto.amount;
