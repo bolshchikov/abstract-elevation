1296e7f95c54e858e9b90d7c7e5d706beb060070
Sergey Bolshchikov <sergey@bolshchikov.net>
2022-12-26 09:19:28 +0200

    chore(guinea-pig-nest): remove telemetry from invoices module

--- guinea-pig-nest/src/invoices/invoices.module.ts
+++ guinea-pig-nest/src/invoices/invoices.module.ts
@@ -1,24 +1,8 @@
 import { Module } from '@nestjs/common';
-import { OpenTelemetryModule } from 'nestjs-otel';
 import { IdsService } from '../ids/ids.service';
+import { StorageService } from '../storage/storage.service';
 import { InvoicesController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
-import { StorageService } from '../storage/storage.service';
-
-const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
-  metrics: {
-    hostMetrics: true, // Includes Host Metrics
-    apiMetrics: {
-      enable: true, // Includes api metrics
-      defaultAttributes: {
-        // You can set default labels for api metrics
-        custom: 'label',
-      },
-      ignoreRoutes: ['/favicon.ico'], // You can ignore specific routes (See https://docs.nestjs.com/middleware#excluding-routes for options)
-      ignoreUndefinedRoutes: false, //Records metrics for all URLs, even undefined ones
-    },
-  },
-});
 
 @Module({
   imports: [],
