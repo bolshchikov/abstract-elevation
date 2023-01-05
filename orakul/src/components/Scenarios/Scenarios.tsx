e86471dd43fc64afc97791ae77ea56f8607e4797
Sergey Bolshchikov <sergey@bolshchikov.net>
2023-01-05 17:04:11 +0200

    fix(orakul): filter out controller methods that are not api handlers

--- orakul/src/components/Scenarios/Scenarios.tsx
+++ orakul/src/components/Scenarios/Scenarios.tsx
@@ -4,30 +4,35 @@ const isController = (file) => file.name.endsWith('.controller');
 const isResolver = (file) => file.name.endsWith('.resolver');
 
 const Scenarios = ({ onChange }) => {
-  const controllers = [...Object.values(staticRaw).filter(isController), ...Object.values(staticRaw).filter(isResolver)];
+  const controllers = [
+    ...Object.values(staticRaw).filter(isController),
+    ...Object.values(staticRaw).filter(isResolver)
+  ];
   return (
     <>
       <h4>APIs</h4>
       {controllers.map(({ name, exports }, idx) => {
-        const members = exports[0].members;
+        const members: any[] = exports[0].members;
         return (
           <div key={idx}>
             <h5>{name}</h5>
-            {members.map(member => {
-              const scenario = `${member.method} ${isResolver({ name }) ? member.name : member.apiPath}`;
-              return (
-                <div key={scenario} className="ScenarioSelector">
-                  <input
-                    name="scenario-selector"
-                    id={scenario}
-                    type="radio"
-                    value={scenario}
-                    onClick={onChange}
-                  />
-                  <label htmlFor={scenario}>{scenario}</label>
-                </div>
-              )
-            })}
+            {members
+              .filter((member) => Boolean(member.method))
+              .map(member => {
+                const scenario = `${member.method} ${isResolver({ name }) ? member.name : member.apiPath}`;
+                return (
+                  <div key={scenario} className="ScenarioSelector">
+                    <input
+                      name="scenario-selector"
+                      id={scenario}
+                      type="radio"
+                      value={scenario}
+                      onClick={onChange}
+                    />
+                    <label htmlFor={scenario}>{scenario}</label>
+                  </div>
+                )
+              })}
           </div>
         )
       })}
